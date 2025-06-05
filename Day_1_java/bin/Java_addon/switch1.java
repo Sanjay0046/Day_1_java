8d7558abe64e696fba85f02ab5f01862c1f97935
Sanjay0046 <manisanjay914@gmail.com>
2025-06-05 15:20:35 +0530

    Add files via upload

---
+++ Day_1_java/bin/Java_addon/switch1.java
@@ -0,0 +1,27 @@
+package Java_addon;
+
+public class switch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int day=2;
+		
+		switch (day) {
+		case 1:
+			System.out.println("monday");
+			break;
+		case 2:
+			System.out.println("tuesday");
+			break;
+		case 3:	
+			System.out.println("saturday");
+			break;
+		default:
+			System.out.println("sunday");
+			break;
+		}
+
+	}
+
+}
